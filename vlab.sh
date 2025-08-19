a8ef0124464630c71803b0bc7e76b29243145d3d
Paradox <>
2025-08-20 00:34:00 +0300

    feat: new menu

--- vlab.sh
+++ vlab.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 # vlab controller. detects os, defines installers, runs menu.
 
+: '
+Thoughts about design:
+Start typing index to highightor send the letters
+Search work in subcatalogues also. All is indexed. 
+
+Menu transition 
+| 0. Tools |                         | 1. Cracking|
+    |                                      | -- 0. Back 
+    | -- 1. Recon                          | -- 2. Core  
+    | -- 2. Wifi       --->                | -- 3. Cloud   
+    | -- 3. Cracking 
+
+or 
+vlab toolkin // os: arch
+use arrow keys to navigate, enter to select, 'q' to go back/quit.
+
+ > Install
+   Diagnostics
+
+------>
+
+> Pkg1
+> Pkg2
+> ...
+
+
+Highlighting works like:
+
+1 version               or  2 version:
+| -- 1. Recon               | -- 1. Recon                 
+| -- 2. Wifi                | -- 2. Wifi
+-------------------         | -- (s) 3. Cracking 
+| -- 3. Cracking |          | -- 4. Something else
+-------------------
+| -- 4. Something else
+'
+
 # --- core setup
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source_dir="$script_dir/definitions"
@@ -55,31 +92,137 @@ prepare_system() {
     print_success "system updated and core dependencies are present."
 }
 
-# --- main menu
-prepare_system
-while true; do
-    clear
-    echo -e "${C_GREEN}vlab toolkit // os: $vlab_os${C_RESET}"
-    echo "--- install & setup ---"
-    PS3=$'\n> '
-    options=(
-        "recon tools" "wifi tools" "general tools" "evm tools" "docker engine"
-        "--- diagnostics ---" "check installed" "verify sandbox" "--- exit ---" "quit"
-    )
-    select opt in "${options[@]}"; do
-        case $opt in
-            "recon tools") bash "$modules_dir/install_tools.sh" recon; break;;
-            "wifi tools") bash "$modules_dir/install_tools.sh" wifi; break;;
-            "general tools") bash "$modules_dir/install_tools.sh" general; break;;
-            "evm tools") bash "$modules_dir/install_tools.sh" evm; break;;
-            "docker engine") bash "$modules_dir/install_tools.sh" docker; break;;
-            "check installed") bash "$modules_dir/diagnostics.sh" check; continue 2;;
-            "verify sandbox") bash "$modules_dir/diagnostics.sh" verify; continue 2;;
-            "quit") print_info "exit."; exit 0;;
-            *) print_error "invalid option.";;
+# --- interactive menu renderer
+# args: 1. title, 2. prompt, 3. options array, 4. descriptions array
+render_menu() {
+    local title="$1"
+    local prompt="$2"
+    local -n options=$3
+    local -n descriptions=$4
+    local current_selection=0
+
+    # hide cursor
+    tput civis
+    # trap ctrl-c and restore cursor
+    trap "tput cnorm; exit" SIGINT
+
+    while true; do
+        clear
+        echo -e "${C_GREEN}$title${C_RESET}"
+        echo "use arrow keys to navigate, enter to select, 'q' to go back/quit."
+        echo ""
+
+        for i in "${!options[@]}"; do
+            if [ "$i" -eq "$current_selection" ]; then
+                echo -e " > ${C_YELLOW}${options[$i]}${C_RESET}"
+            else
+                echo "   ${options[$i]}"
+            fi
+        done
+
+        echo -e "\n${C_BLUE}description:${C_RESET}"
+        # print description in a fixed-size box
+        printf "%s\n" "${descriptions[$current_selection]}" | fold -s -w 80
+
+        read -rsn1 key
+        case "$key" in
+            # ansi escape for arrow up
+            $'\x1b')
+                read -rsn2 key
+                if [[ $key == "[A" ]]; then
+                    current_selection=$(( (current_selection - 1 + ${#options[@]}) % ${#options[@]} ))
+                elif [[ $key == "[B" ]]; then
+                    current_selection=$(( (current_selection + 1) % ${#options[@]} ))
+                fi
+                ;;
+            # enter key
+            "")
+                tput cnorm # restore cursor
+                return $current_selection
+                ;;
+            # q for quit
+            q|Q)
+                tput cnorm
+                return 255 # special exit code for 'back'
+                ;;
         esac
     done
-    print_success "task finished. relog or source shell profile to apply changes."
-    read -n 1 -s -r -p "press any key to exit."
-    exit 0
+}
+
+prepare_system() { print_info "syncing packages and installing core tools..."; os_update; os_pkg_install "${core_deps[@]}"; }
+prepare_system
+
+while true; do
+    # discover main menu categories from directory names
+    mapfile -t categories < <(find "$modules_dir" -mindepth 1 -maxdepth 1 -type d -printf "%f\n" | sort)
+    category_names=()
+    for cat in "${categories[@]}"; do
+        # format for categories, ie : '1_install' to 'Install'
+        category_names+=("$(tr '_' ' ' <<< "${cat//[0-9_]/ }" | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1')")
+    done
+
+    render_menu "${C_GREEN}vlab toolkit // os: $vlab_os${C_RESET}" "select a category" category_names categories
+    main_choice=$?
+
+    if [ $main_choice -eq 255 ]; then clear; print_info "exit."; exit 0; fi
+
+    selected_category_dir="$modules_dir/${categories[$main_choice]}"
+
+    # discover modules in the selected category
+    mapfile -t module_files < <(find "$selected_category_dir" -maxdepth 1 -type f -name "*.sh" -printf "%f\n" | sort)
+    module_names=()
+    module_descs=()
+    for file in "${module_files[@]}"; do
+        # extract metadata from module files
+        name=$(grep '^# META_NAME=' "$selected_category_dir/$file" | cut -d'=' -f2 | tr -d '"')
+        desc=$(grep '^# META_DESC=' "$selected_category_dir/$file" | cut -d'=' -f2 | tr -d '"')
+        module_names+=("${name:-$file}")
+        module_descs+=("${desc:-no description available.}")
+    done
+
+    render_menu "${category_names[$main_choice]}" "select a module to run" module_names module_descs
+    sub_choice=$?
+
+    if [ $sub_choice -eq 255 ]; then
+        continue # go back to main menu
+    fi
+
+    selected_module="$selected_category_dir/${module_files[$sub_choice]}"
+    clear
+    print_info "executing module: ${module_names[$sub_choice]}"
+    echo "-----------------------------------------------------"
+    # execute the chosen module
+    bash "$selected_module"
+    echo "-----------------------------------------------------"
+    print_info "module finished. press any key to return to the menu."
+    read -rsn1
 done
+
+# --- main menu
+# prepare_system
+# while true; do
+#     clear
+#     echo -e "${C_GREEN}vlab toolkit // os: $vlab_os${C_RESET}"
+#     echo "--- install & setup ---"
+#     PS3=$'\n> '
+#     options=(
+#         "recon tools" "wifi tools" "general tools" "evm tools" "docker engine"
+#         "--- diagnostics ---" "check installed" "verify sandbox" "--- exit ---" "quit"
+#     )
+#     select opt in "${options[@]}"; do
+#         case $opt in
+#             "recon tools") bash "$modules_dir/install_tools.sh" recon; break;;
+#             "wifi tools") bash "$modules_dir/install_tools.sh" wifi; break;;
+#             "general tools") bash "$modules_dir/install_tools.sh" general; break;;
+#             "evm tools") bash "$modules_dir/install_tools.sh" evm; break;;
+#             "docker engine") bash "$modules_dir/install_tools.sh" docker; break;;
+#             "check installed") bash "$modules_dir/diagnostics.sh" check; continue 2;;
+#             "verify sandbox") bash "$modules_dir/diagnostics.sh" verify; continue 2;;
+#             "quit") print_info "exit."; exit 0;;
+#             *) print_error "invalid option.";;
+#         esac
+#     done
+#     print_success "task finished. relog or source shell profile to apply changes."
+#     read -n 1 -s -r -p "press any key to exit."
+#     exit 0
+# done
